8f097474f4c60f9296e6254e4216eab0cf66d240
gtpesantren22 <cakromat26@gmail.com>
2022-07-31 12:00:51 +0700

    tambah fitur  register

--- foot.php
+++ foot.php
@@ -1,8 +1,8 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-        <b>Version</b> 2.3.0
+        <b>Version</b> 1.0.0
     </div>
-    <strong>Copyright &copy; 2014-2015 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2022/2023 <a href="#">Admin DWK</a>.</strong>
 </footer>
 
 
---
+++ register.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 2 | Registration Page</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.5 -->
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="plugins/iCheck/square/blue.css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body class="hold-transition register-page">
+  <div class="register-box">
+    <div class="register-logo">
+      <a href="index2.html"><b>Admin</b>LTE</a>
+    </div>
+
+    <div class="register-box-body">
+      <p class="login-box-msg">Register a new membership</p>
+      <form action="" method="post">
+        <div class="form-group has-feedback">
+          <input type="text" class="form-control" placeholder="Full name" name="nama" required>
+          <span class="glyphicon glyphicon-user form-control-feedback"></span>
+        </div>
+        <div class="form-group has-feedback">
+          <input type="text" class="form-control" placeholder="Username" name="user" required>
+          <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        </div>
+        <div class="form-group has-feedback">
+          <input type="password" class="form-control" placeholder="Password" name="pass" required>
+          <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+        </div>
+        <div class="row">
+          <div class="col-xs-8">
+            <div class="checkbox icheck">
+              <label>
+                <input type="checkbox"> I agree to the <a href="#">terms</a>
+              </label>
+            </div>
+          </div><!-- /.col -->
+          <div class="col-xs-4">
+            <button type="submit" name="daftar" class="btn btn-primary btn-block btn-flat">Register</button>
+          </div><!-- /.col -->
+        </div>
+      </form>
+
+
+      <a href="login.php" class="text-center">I already have a membership</a>
+    </div><!-- /.form-box -->
+  </div><!-- /.register-box -->
+
+  <!-- jQuery 2.1.4 -->
+  <script src="plugins/jQuery/jQuery-2.1.4.min.js"></script>
+  <!-- Bootstrap 3.3.5 -->
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+  <!-- iCheck -->
+  <script src="plugins/iCheck/icheck.min.js"></script>
+  <script>
+    $(function() {
+      $('input').iCheck({
+        checkboxClass: 'icheckbox_square-blue',
+        radioClass: 'iradio_square-blue',
+        increaseArea: '20%' // optional
+      });
+    });
+  </script>
+</body>
+
+</html>
+
+<?php
+include 'koneksi.php';
+
+if (isset($_POST['daftar'])) {
+
+  $nama = mysqli_real_escape_string($conn, $_POST['nama']);
+  $user = addslashes(strtolower(mysqli_real_escape_string($conn, $_POST['user'])));
+  $pass = addslashes(strtolower(mysqli_real_escape_string($conn, $_POST['pass'])));
+  $passOk = password_hash($pass, PASSWORD_DEFAULT);
+
+  $sql = mysqli_query($conn, "SELECT * FROM user WHERE username = '$user' ");
+
+  if (mysqli_num_rows($sql) > 0) {
+    echo "
+            <script>
+            alert('Username sudah terpakai');
+            window.location = 'regist.php';
+            </script>
+            ";
+  } else {
+    $ss = mysqli_query($conn, "INSERT INTO user VALUES ('', '$nama', '$user', '$passOk', 'admin', 'T') ");
+    if ($ss) {
+      echo "
+            <script>
+            alert('Akun sudah masuk. Untuk aktifasi silahkan menghubungi admin');
+            window.location = 'login.php';
+            </script>
+            ";
+    }
+  }
+}
+
+?>
